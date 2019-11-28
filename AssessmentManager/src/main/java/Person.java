3a71c97056e6b4df1330c8897158eb6d036b45e2
LeMikado <micscarito@gmail.com>
2019-11-28 10:52:39 +0100

    up

--- AssessmentManager/src/main/java/Person.java
+++ AssessmentManager/src/main/java/Person.java
@@ -1,13 +1,33 @@
 import java.io.Serializable;
 
+/**
+ *
+ */
 public class Person implements Serializable {
-    final int ID;
+    /**
+     *
+     */
+    final int id;
+    /**
+     *
+     */
     final String firstName;
+    /**
+     *
+     */
     final String lastName;
+    /**
+     *
+     */
     private Address address;
 
-    public Person(int ID, String firstName, String lastName) {
-        this.ID = ID;
+    /**
+     * @param id
+     * @param firstName
+     * @param lastName
+     */
+    public Person(final int id, final String firstName, final String lastName) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
     }
