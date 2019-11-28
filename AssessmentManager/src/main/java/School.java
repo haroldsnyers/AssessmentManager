1d8f3e2cb6bb3d7cf21a9041de857ccf85e888cd
Oscar <oscar.marafko@gmail.com>
2019-11-28 11:23:51 +0100

    School whitespace checkstyle

--- AssessmentManager/src/main/java/School.java
+++ AssessmentManager/src/main/java/School.java
@@ -6,23 +6,23 @@ public class School implements Serializable {
 	  public static String name;
 	  private static String address;	
 	  //private static Teacher ListTeacher[];
-	  public Map<Integer,Student>studentsDico;
-	  public Map<Integer,Teacher>teachersDico;
+	  public Map<Integer, Student>studentsDico;
+	  public Map<Integer, Teacher>teachersDico;
 
 	  public School(String name, String address)
 	  {  
 		  this.name = name;
 		  this.address = address;
-		  this.studentsDico=new HashMap<Integer,Student>();
-		  this.teachersDico=new HashMap<Integer,Teacher>();
+		  this.studentsDico = new HashMap<Integer, Student>();
+		  this.teachersDico = new HashMap<Integer, Teacher>();
 		  //this.ListTeacher = ListTeacher;			  
 	  }
 	  
 	  public void addTeacher(Teacher teach) {
-		  this.teachersDico.put(teach.id,teach);
+		  this.teachersDico.put(teach.id, teach);
 	  }
 	  
 	  public void addStudent(Student stud) {
-		  this.studentsDico.put(stud.ID_number,stud);
+		  this.studentsDico.put(stud.ID_number, stud);
 	  }
 }
