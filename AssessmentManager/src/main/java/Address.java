1fbc9a0a7349c512b20e1a77a58d57e8adf35d22
LeMikado <micscarito@gmail.com>
2019-11-28 11:07:44 +0100

    up

--- AssessmentManager/src/main/java/Address.java
+++ AssessmentManager/src/main/java/Address.java
@@ -1,16 +1,65 @@
 import java.io.Serializable;
 
+/**
+ *
+ */
 public class Address implements Serializable {
-    public String city;
-    public int postalCode;
-    public String street;
+    /**
+     *
+     */
+    private String city;
+    /**
+     *
+     */
+    private int postalCode;
+    /**
+     *
+     */
+    private String street;
+    /**
+     *
+     */
     private int number;
     //public List<Course> courseList;
 
-    public Address(String city, int postalCode, String street, int number) {
+    /**
+     * @param city
+     * @param postalCode
+     * @param street
+     * @param number
+     */
+    public Address(final String city, final int postalCode, final String street, final int number) {
         this.city = city;
         this.postalCode = postalCode;
         this.street = street;
         this.number = number;
+    }
+
+    /**
+     * @return
+     */
+    public String getCity() {
+        return this.city;
+    }
+
+    /**
+     * @return
+     */
+    public int getPostalCode() {
+        return this.postalCode;
+    }
+
+    /**
+     * @return
+     */
+    public String getStreet() {
+        return this.street;
+    }
+
+    /**
+     * @return
+     */
+    public int getNumber() {
+        return this.number;
     }
 }
