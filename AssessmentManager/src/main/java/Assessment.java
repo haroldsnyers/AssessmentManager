7737f7f18827416e251702f9b10c9579939f7bcc
Oscar <oscar.marafko@gmail.com>
2019-11-28 11:34:10 +0100

    Assessment checkstyle

--- AssessmentManager/src/main/java/Assessment.java
+++ AssessmentManager/src/main/java/Assessment.java
@@ -1,9 +1,22 @@
 import java.io.Serializable;
 
+/**
+ *
+ */
 public class Assessment implements Serializable {
+    /**
+     *
+     */
     private static float Result;
+    /**
+     *
+     */
     private static String AcademicYear;
 
+    /**
+     * @param Result       
+     * @param AcademicYear
+     */
     public Assessment(final float Result, final String AcademicYear) {
         this.Result = Result;
         this.AcademicYear = AcademicYear;
