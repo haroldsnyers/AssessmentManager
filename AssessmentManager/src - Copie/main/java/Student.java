ad4e1db8b8383ba313a56d9ddd2d2f7181468ae8
Theo.ducoul <Dtheo98@gmail.com>
2019-12-05 10:46:29 +0100

    Revert "restructuration"
    This reverts commit 34a9e9739cd07d147f489f8146f5e2d0aa88503d.

---
+++ AssessmentManager/src - Copie/main/java/Student.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ */
+public class Student extends Person {
+
+	/**
+	 * @param id receive id
+	 * @param name receive name
+	 * @param surname receive surname
+	 */
+	public Student(final int id, final String name, final String surname) {
+		super(id, name, surname);
+	}
+}
